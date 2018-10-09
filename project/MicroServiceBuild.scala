bac27360f95f2e7d7c671a9d943c6ea7f0e719a1
Thomas Rynne <thomas.rynne@digital.hmrc.gov.uk>
2018-10-09 17:26:50 +0100

    [TR] TXM-3285 upgrade for bobby

--- project/MicroServiceBuild.scala
+++ project/MicroServiceBuild.scala
@@ -22,7 +22,7 @@ private object AppDependencies {
 
   val compile = Seq(
     ws,
-    "uk.gov.hmrc" %% "microservice-bootstrap" % "6.9.0",
+    "uk.gov.hmrc" %% "microservice-bootstrap" % "6.19.0",
     "uk.gov.hmrc" %% "play-ui" % "7.13.0",
     "uk.gov.hmrc" %% "domain" % "5.0.0",
     "uk.gov.hmrc" %% "logging" % "0.4.0" withSources(),
