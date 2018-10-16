cc2a33a078a152670cb868d94ff56e9d4092d72c
Thomas Rynne <thomas.rynne@digital.hmrc.gov.uk>
2018-10-16 17:29:02 +0100

    [TR] TXM-3285 use artifact created by the new build server

--- project/MicroServiceBuild.scala
+++ project/MicroServiceBuild.scala
@@ -24,7 +24,7 @@ private object AppDependencies {
     "uk.gov.hmrc" %% "play-ui" % "7.13.0",
     "uk.gov.hmrc" %% "domain" % "5.0.0",
     "uk.gov.hmrc" %% "logging" % "0.4.0" withSources(),
-    "uk.gov.hmrc" %% "address-reputation-store" % "2.28.0" withSources(),
+    "uk.gov.hmrc" %% "address-reputation-store" % "2.33.0" withSources(),
     "uk.gov.hmrc" %% "play-random-json-filter" % "0.3.0",
     "com.univocity" % "univocity-parsers" % "1.5.6",
     "com.fasterxml.jackson.core" % "jackson-core" % jacksonVersion,
